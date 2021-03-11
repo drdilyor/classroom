ef3a0d9b47bddd1196fa5a7ae56781522f13dd3c
drdilyor <drdilyor@outlook.com>
2021-03-11 18:30:09 +0500

    Add: basic routes

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,36 @@
-from fastapi import FastAPI
+import asyncio
+from typing import List
 
+from fastapi import FastAPI, HTTPException
+
+import models
 from models import *
+import schemas as s
 
 
 app = FastAPI()
+asyncio.create_task(models.init())
+
+
+def e404():
+    raise HTTPException(404)
+
 
 @app.get('/')
 async def index():
     return {'message': 'Hello world'}
+
+
+@app.get('/courses', response_model=List[s.Course])  # noqa
+async def all_courses():
+    return await Course.all()
+
+
+@app.get('/courses/{course_id}', response_model=s.CourseDetailed)
+async def get_course(course_id: int):
+    qs = Course.filter(id=course_id).prefetch_related('course_parts')
+    c: Course = await qs.get_or_none() or e404()
+    return {
+        **c.__dict__,
+        'course_parts': await c.course_parts,  # noqa
+    }
