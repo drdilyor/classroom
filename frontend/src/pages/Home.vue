a40871aba35d0b8141523ef72c42cb8415b86c74
drdilyor <drdilyor@outlook.com>
2021-03-12 20:18:29 +0500

    Add: Home.vue

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,3 +1,17 @@
 <template>
-  <p>Home</p>
+  <div class="container">
+    <h1>Welcome to &lt;A&gt;Code</h1>
+    <p>An open source E-Learning platform!</p>
+    <p>
+      <router-link class="btn btn-accent waves-effect waves-light" to="/courses">
+        Explore Courses<i class="material-icons right">arrow_forward</i>
+      </router-link>
+    </p>
+  </div>
 </template>
+
+<script>
+export default {
+  
+}
+</script>
