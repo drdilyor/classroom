5ceb87de0b0242dedc0529ae24a5c55e7c007439
drdilyor <drdilyor@outlook.com>
2021-03-12 19:43:13 +0500

    Add: Header.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,22 +11,18 @@
 <script>
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
+import M from 'materialize-css'
 
 export default {
   name: 'App',
   components: {
     Header, Footer
+  },
+  mounted() {
+    M.AutoInit()
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav>
+    <div class="nav-wrapper">
+      <router-link to="/" class="brand-logo center">&lt;A&gt;Code</router-link>
+      <ul id="nav-mobile" class="left">
+        <li><router-link to="/courses">Explore</router-link></li>
+        <li><router-link to="/classroom">Classroom</router-link></li>
+      </ul>
+      <ul class="right">
+        <li><router-link to="/signup">Sign up</router-link></li>
+      </ul>
+    </div>
+  </nav>
+</template>
