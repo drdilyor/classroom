49bc041b3418799b404d665cf43cd3114ef2ddd2
drdilyor <drdilyor@outlook.com>
2021-03-12 18:01:41 +0500

    Add: basic frontend setup

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+import 'materialize-css/dist/css/materialize.min.css'
+
 import App from './App.vue'
+import router from './routes'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ frontend/src/pages/404.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page-404">
+    <div>
+      <h1>Page not found!</h1>
+      <p>Go to <router-link to="/">main page</router-link></p>
+    </div>
+  </div>
+</template>
+
+<style>
+  .page-404 {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 60vh;
+  }
+</style>
