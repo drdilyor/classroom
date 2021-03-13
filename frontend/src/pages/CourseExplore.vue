f9c6ad5f0e87ffa38f8f83ff5033f45e77369946
drdilyor <drdilyor@outlook.com>
2021-03-13 16:04:42 +0500

    Add: Preloader in CourseExplore.vue

--- frontend/src/pages/CourseExplore.vue
+++ frontend/src/pages/CourseExplore.vue
@@ -1,32 +1,52 @@
 <template>
   <div class="container">
     <h1>Courses</h1>
-    <div class="card horizontal" v-for="course in courses" :key="course.id">
-      <div class="card-image">
-        <img :src="course.image_link">
-      </div>
-      <div class="card-stacked">
-        <div class="card-content">
-          <span class="card-title">{{ course.title }}</span>
-          <p>{{ course.description }}</p>
-        </div>
-        <div class="card-action">
-          <a href="#">Details</a>
-        </div>
-      </div>
+    <div v-if="loading">
+      <div class="progress"><div class="indeterminate"></div></div>
+      <p>Loading...</p>
     </div>
+    <p v-else-if="error">
+      <strong>Error</strong>: something went wrong :(
+    </p>
+    <template v-else>
+      <div class="card horizontal"
+        v-for="course in courses"
+        :key="course.id">
+        <div class="card-image">
+          <img :src="course.image_link">
+        </div>
+        <div class="card-stacked">
+          <div class="card-content">
+            <span class="card-title">{{ course.title }}</span>
+            <p>{{ course.description }}</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Details</a>
+          </div>
+        </div>
+      </div>
+    </template>
   </div>
 </template>
 
 <script>
 export default {
   data() { return {
-    courses: []
+    courses: [],
+    loading: true,
+    error: false,
   } },
   mounted() {
     fetch(this.$backend + '/courses')
     .then(res => res.json())
-    .then(data => this.courses = data)
+    .then(data => {
+      this.courses = data
+      this.loading = false
+    })
+    .catch(err => {
+      this.error = err
+      this.loading = false
+    })
   },
 }
 </script>
