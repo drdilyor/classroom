5189af837aa6049845d0ef4f20ec191c176a8690
drdilyor <drdilyor@outlook.com>
2021-04-10 19:23:58 +0500

    CourseExplore

--- frontend/src/pages/CourseExplore.vue
+++ frontend/src/pages/CourseExplore.vue
@@ -9,18 +9,16 @@
       <strong>Error</strong>: something went wrong :(
     </p>
     <template v-else>
-      <div class="card horizontal"
+      <div class="explore-tile"
         v-for="course in courses"
         :key="course.id">
-        <div class="card-image">
+        <div class="explore-tile-image">
           <img :src="course.image_link">
         </div>
-        <div class="card-stacked">
-          <div class="card-content">
-            <span class="card-title">{{ course.title }}</span>
-            <p>{{ course.description }}</p>
-          </div>
-          <div class="card-action">
+        <div class="explore-tile-body">
+          <span class="explore-tile-title">{{ course.title }}</span>
+          <p class="flex-grow-1">{{ course.description }}</p>
+          <div class="explore-tile-action">
             <router-link :to="`/courses/${course.id}`">Details</router-link>
           </div>
         </div>
@@ -51,12 +49,57 @@ export default {
 }
 </script>
 
-<style scoped>
-.card {
-  height: 14rem;
+<style lang="scss">
+.explore-tile {
+  height: 20rem;
+  margin-bottom: 1rem;
+  border-radius: 4px;
+  box-shadow: 0 .25rem .5rem #00000044;
+
+  &-image {
+    height: 50%;
+    img {
+      height: 100%;
+      width: 100%;
+      object-fit: cover;
+      border-top-left-radius: 4px;
+      border-top-right-radius: 4px;
+    }
+  }
+  
+  &-body {
+    display: flex;
+    height: 50%;
+    flex-direction: column;
+    padding: .5rem;
+  }
+  &-title {
+    font-size: 1.6rem;
+  }
+  &-action {
+    padding: .5rem;
+    border-top: 1px solid #00000044;
+    a {
+      text-decoration: none;
+      color: var(--bs-blue);
+    }
+  }
 }
-.card-image,
-.card-image img {
-  height: 100%;
+
+@media screen and (min-width: 992px) {
+  .explore-tile {
+    display: flex;
+    flex-direction: row;
+    height: 14rem;
+    &-image {
+      height: 100%;
+      width: 20rem
+    }
+    &-body {
+      height: 100%;
+      flex-grow: 1;
+    }
+  }
 }
+
 </style>
