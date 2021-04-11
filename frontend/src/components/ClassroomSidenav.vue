f792ead09c017839e47def4e4d73fa67ee57abae
drdilyor <drdilyor@outlook.com>
2021-04-11 18:48:15 +0500

    ClassroomHome

--- frontend/src/components/ClassroomSidenav.vue
+++ frontend/src/components/ClassroomSidenav.vue
@@ -1,119 +1,63 @@
 <template>
-  <div class="nav-groups" :class="{small}">
-    <ul>
-      <li>
-        <router-link class="brand-logo" to="/" target="_blank">
-          <img v-if="small" src="/assets/img/logo-small.png" alt="&lt;A&gt;">
-          <img v-else src="/assets/img/logo.png" alt="&lt;A&gt;Code">
-        </router-link>
-      </li>
-      <li>
-        <router-link to="/classroom">
-          <i class="material-icons">home</i>
-          <span>Home</span>
-        </router-link>
-      </li>
-      <li>
-        <router-link to="/courses">
-          <i class="material-icons">dashboard</i>
-          <span>Catalog</span>
-        </router-link>
-      </li>
-    </ul>
-    <ul>
-      <li>
-        <router-link to="/classroom/settings">
-          <i class="material-icons">settings</i>
-          <span>Settings</span>
-        </router-link>
-      </li>
-      <li>
-        <a href="javascript:void(0)" @click="logout">
-          <i class="material-icons">arrow_backward</i>
-          <span>Log out</span>
-        </a>
-      </li>
-    </ul>
+  <div
+    class="sidenav"
+    :class="{show}" :style="{visibility: show}">
+    <slot>
+      
+    </slot>
   </div>
 </template>
 
 <script>
 export default {
   props: {
-    small: {
+    show: {
       type: Boolean,
-      default: false,
+      default: true,
     }
   },
   methods: {
     logout() {
       this.$auth.logout()
-      window.location = '/'
+      windOw.location = '/'
     }
   }
 }
 </script>
 
-<style scoped>
-.nav-groups {
-  min-height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: stretch;
+<style>
+
+/* (1) Disable visibility *while* transiting */
+.sidenav {
+  z-index: 1040;
+  width: 0;
+  border-right: 1px solid rgba(0, 0, 0, .125  );
+  visibility: hidden;
+  background: white;
+  transition: width .3s; /* (1) */
 }
 
-.nav-groups > ul:first-child {
-  flex: 1;
-}
-.nav-groups > ul {
-  margin-top: 0;
-  margin-bottom: 0;
-}
-
-.brand-logo {
-  font-size: 1.6em;
-}
-a {
-  display: flex;
-}
-a span:last-child {
-  flex: 1;
+@media screen and (max-width: 991.9px) {
+  /* .sidenav {
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    width: 0;
+  } */
+  .sidenav.show {
+    width: 18rem;
+    visibility: visible;
+    transition: width .3s, visibility 0s .3s; /* (1) */
+  }
 }
 
-.small {
-  width: 48px;
+@media screen and (min-width: 992px) {
+  .sidenav {
+    width: 18rem;
+    height: 100%;
+    visibility: visible;
+    transition: width .3s, visibility 0s .3s; /* (1) */
+  }
 }
-
-.small .brand-logo {
-  font-size: 1.3em;
-}
-
-.small li,
-.small a {
-  width: 48px !important;
-  height: 48px !important;
-}
-.small a {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 0;
-  padding-top: 8px;
-}
-.small a > * {
-  float: none !important;
-}
-.small .material-icons {
-  height: 24px;
-  margin: 0 !important;
-  float: none !important;
-  line-height: 24px;
-}
-.small a span {
-  height: 16px;
-  font-size: 10px;
-  line-height: 16px;
-  text-transform: uppercase;
-}
-
 </style>
