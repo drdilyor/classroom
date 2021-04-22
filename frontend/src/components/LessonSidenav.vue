95c75621f3888a2e641fb949baa99592d29be99e
drdilyor <drdilyor@outlook.com>
2021-04-22 23:39:06 +0500

    Improved lesson sidenav

--- frontend/src/components/LessonSidenav.vue
+++ frontend/src/components/LessonSidenav.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="bg-light lesson-sidenav">
+  <div class="bg-light lesson-sidenav" v-if="course">
     <router-link
       tag="a"
-      :to="'/classroom/course/' + course_id"
+      :to="'/classroom/course/' + part.course_id"
       class="border-bottom lesson-sidenav-header"
       style="cursor: pointer"
     >
       <i class="material-icons p-0 text-secondary" style="font-size: 64px">arrow_left</i>
       <div class="d-flex align-items-center flex-grow-1">
-        <h2>{{ course_part_title }}</h2>
+        <h2>{{ title }}</h2>
       </div>
     </router-link>
     <ul v-if="part" class="lesson-list">
@@ -21,7 +21,7 @@
         <i class="material-icons lesson-status" :class="{done: lesson.is_viewed}">
           {{ lesson.is_viewed ? 'done' : 'circle' }}
         </i>
-        <span class="lesson">{{ lesson.title }}</span>
+        <span class="lesson overflow-hidden">{{ lesson.title }}</span>
       </router-link>
     </ul>
   </div>
@@ -30,13 +30,6 @@
 <script>
 export default {
   props: {
-    course_part_title: {
-      default: 'Lesson 1'
-    },
-    course_id: {
-      type: Number,
-      default: 1,
-    },
     lesson: {
       default() { return {
         title: 'blah',
@@ -47,6 +40,12 @@ export default {
     part() {
       return this.$store.getters.currentPart
     },
+    title() {
+      return this.part.title
+    },
+    course() {
+      return this.$store.getters.currentCourse
+    }
   },
   destroyed() {
     console.error('hello');
@@ -78,21 +77,28 @@ export default {
   li {
     display: flex;
     align-items: center;
-    height: 5em;
+    height: 3em;
     border-left: 4px solid transparent;
+    color: rgba(0, 0, 0, .5);
     cursor: pointer;
+    transition: color .2s;
+    &:hover {
+      color: rgba(0, 0, 0, .7);
+    }
     &.active {
-      border-left-color: var(--bs-primary)
+      border-left-color: var(--bs-primary);
+      color: black;
     }
   }
   .lesson-status {
+    flex-shrink: 0;
     width: 48px;
     font-size: 12px;
     text-align: center;
     color: var(--bs-gray);
     &.done {
       font-size: 24px;
-      color: var(--bs-primary);
+      color: var(--bs-success);
     }
   }
 }
