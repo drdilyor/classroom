203578b6855c2a25c6a5826e12d1a31f1eb44409
drdilyor <drdilyor@outlook.com>
2021-04-25 20:22:26 +0500

    Header mobiled

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,30 +1,82 @@
 <template>
   <nav class="navbar navbar-expand-md navbar-light bg-light mb-4">
     <div class="container-fluid">
-      <router-link to="/" class="navbar-brand">&lt;A&gt;Code</router-link>
-      <div class="collapse navbar-collapse">
-        <ul class="navbar-nav me-auto mb-lg-0">
-          <li v-for="link in navLinks" :key="link.to" class="nav-item">
-            <router-link :to="link.to" class="nav-link" exact-active-class="active">{{ link.name }}</router-link>
-          </li>
-        </ul>
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <router-link to="/profile" class="nav-link" exact-active-class="active">Profile</router-link>
-          </li>
-        </ul>
-      </div>
+      <router-link to="/" class="navbar-brand ps-2">&lt;A&gt;Code</router-link>
+      <toggler @click="collapse = !collapse" :on="collapse" class="me-2 d-md-none" />
+      <transition name="collapse">
+        <div class="navbar-collapse" v-show="collapse" @click="navbarClick">
+          <ul class="navbar-nav me-auto mb-lg-0">
+            <li v-for="link in navLinks" :key="link.to" class="nav-item">
+              <router-link
+                :to="link.to"
+                class="nav-link"
+                exact-active-class="active"
+                >{{ link.name }}</router-link>
+            </li>
+          </ul>
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link
+                to="/profile"
+                class="nav-link"
+                exact-active-class="active"
+                >Profile</router-link>
+            </li>
+          </ul>
+        </div>
+      </transition>
     </div>
   </nav>
 </template>
 
 <script>
+import Toggler from "@/components/ui/Toggler.vue";
+
 export default {
-  data() { return {
-    navLinks: [
-      {to: '/courses', name: 'Explore'},
-      {to: '/classroom', name: 'Classroom'},
-    ]
-  } }
+  components: {
+    Toggler,
+  },
+  data() {
+    return {
+      navLinks: [
+        { to: "/courses", name: "Explore" },
+        { to: "/classroom", name: "Classroom" },
+      ],
+      collapse: true,
+    }
+  },
+  methods: {
+    navbarClick(e) {
+      if (e.target.tagName == 'A')
+        this.collapse = false
+    }
+  },
+  mounted() {
+    // Fix: the navbar is not collapsed on refresh
+    this.$nextTick(() => this.collapse = false)
+  }
 }
 </script>
+
+<style>
+@media screen and (max-width: 767.9px) {
+  .navbar > .container-fluid {
+    position: relative;
+  }
+  .navbar-collapse {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: calc(40px * -3);
+    top: 100%;
+    padding-left: 0.5em;
+    background: var(--bs-light);
+    overflow: hidden;
+    transition: bottom 0.5s;
+  }
+  .collapse-enter,
+  .collapse-leave-active {
+    bottom: 0;
+  }
+}
+</style>
---
+++ frontend/src/components/ui/Toggler.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="toggler" :class="{on}" @click="$emit('click', $event)">
+    <div class="bar bar-1"></div>
+    <div class="bar bar-2"></div>
+    <div class="bar bar-3"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    on: {
+      type: Boolean,
+      default: true,
+    }
+  },
+}
+</script>
+
+<style scoped>
+.toggler {
+  position: relative;
+  width: 32px;
+  height: 20px;
+  cursor: pointer;
+}
+.bar {
+  position: absolute;
+  left: 0;
+  right: 0;
+  height: 3px;
+  background: var(--bs-gray);
+  transition: all .3s;
+}
+
+.bar-1 { top: 0 }
+.bar-3 { bottom: 0 }
+.bar-2 {
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+.on .bar-1 {
+  top: 50%;
+  transform: translateY(-50%) rotate(45deg);
+}
+.on .bar-2 {
+  opacity: 0;
+}
+.on .bar-3 {
+  top: 50%;
+  transform: translateY(-50%) rotate(-45deg);
+}
+</style>
