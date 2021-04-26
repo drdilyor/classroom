2f038a140f969ec155d7903d523a2c87f6e1bff7
drdilyor <drdilyor@outlook.com>
2021-04-26 21:26:07 +0500

    ClassroomHome's layout

--- frontend/src/pages/ClassroomHome.vue
+++ frontend/src/pages/ClassroomHome.vue
@@ -1,11 +1,9 @@
 <template>
   <div id="app" class="app-classroom">
-    <classroom-sidenav class="flex-shrink-0" :show="showSidenav" />
-    <div class="flex-grow-1 app-classroom-content">
-      <header class="shadow py-2 mb-4 d-flex">
-        <i class="material-icons ms-2 d-lg-none d-inline-block" @click="showSidenav = !showSidenav">
-          menu
-        </i>
+    <classroom-sidenav :show="showSidenav" />
+    <div class="app-classroom-content">
+      <header>
+        <navbar-burger :is-open="showSidenav" @click="showSidenav = !showSidenav"/>
         <span class="ps-2">
           Home
         </span>
@@ -33,11 +31,13 @@
 </template>
 
 <script>
+import NavbarBurger from 'buefy/src/components/navbar/NavbarBurger.vue'
 import ClassroomCourseCard from '../components/ClassroomCourseCard.vue'
 import ClassroomSidenav from '../components/ClassroomSidenav.vue'
 
 export default {
   components: {
+    NavbarBurger,
     ClassroomCourseCard,
     ClassroomSidenav,
   },
@@ -61,17 +61,70 @@ export default {
 
 <style lang="scss">
 
+// general
 .app-classroom {
-  display: flex;
-  align-items: stretch;
   height: 100%;
-  &-content {
-    overflow: auto;
-    @media screen and (max-width: 992.9px) {
-      flex-shrink: 0; // This breaks the page on mobile
-      max-width: calc(100% - 2px);
+}
+.app-classroom > .sidenav {
+  flex-shrink: 0;
+}
+.app-classroom-content {
+  > header {
+    display: flex;
+    align-items: center;
+  }
+  .navbar-burger {
+    margin-left: 0;
+  }
+}
+
+// mobile
+@media screen and (max-width: 1023.9px) {
+  .app-classroom {
+    display: flex;
+    align-items: stretch;
+    height: 100%;
+
+    &-content {
+      flex: 1 0;
+      min-width: 100%;
+      width: 100%;
     }
   }
+}
+
+// .app-classroom {
+  // display: flex;
+  // align-items: stretch;
+  // height: 100%;
+  // &-content {
+  //   flex-grow: 1;
+  //   overflow: auto;
+  //   @media screen and (max-width: 1023.9px) {
+  //     flex-shrink: 0; // This breaks the page on mobile
+  //     max-width: calc(100% - 2px) !important;
+  //   }
+  //   > header {
+  //     display: flex;
+  //     align-items: center;
+  //     min-height: 3.25rem;
+  //     margin-bottom: 1.5rem;
+  //     // (!) .box's shadow
+  //     box-shadow: 0 0.5em 1em -0.125em rgba(10, 10, 10, 0.1), 0 0px 0 1px rgba(10, 10, 10, 0.02);
+  //     border-radius: 0;
+
+  //     @media screen and (min-width: 1024px) {
+  //       padding-left: 1.25rem;
+  //     }
+  //   }
+  //   .navbar-burger {
+  //     margin-left: 0;
+  //   }
+  // }
+// }
+
+.app-classroom main {
+  padding: 0 1em;
 }
 
 .caps-header {
